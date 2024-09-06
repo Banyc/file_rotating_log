49aeb149c5bfb1b7b6943d41d012306b39009325
Banyc <36535895+Banyc@users.noreply.github.com>
2024-09-06 14:38:55 +0800

    feat: `DailyContains`

--- src/time_past.rs
+++ src/time_past.rs
@@ -3,6 +3,18 @@ use std::sync::Arc;
 pub trait TimeContains: core::fmt::Debug + Sync + Send {
     fn matches(&self, interval: Interval) -> bool;
 }
+#[derive(Debug, Clone)]
+pub struct DailyContains;
+impl TimeContains for DailyContains {
+    fn matches(&self, interval: Interval) -> bool {
+        let Some(exclusive_start) = interval.exclusive_start else {
+            return false;
+        };
+        let start_date = exclusive_start.date();
+        let end_date = interval.inclusive_end.date();
+        start_date != end_date
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct TimePast {
